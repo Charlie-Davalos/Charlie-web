0767a809ba57005640c35667dcd615f4905e215c
Charlie-Davalos <80849047+Charlie-Davalos@users.noreply.github.com>
2021-10-28 08:22:05 -0500

    Add files via upload

---
+++ src/data.js
@@ -0,0 +1,44 @@
+export const projects = [
+  {
+    title: "Mi primer pagina web",
+    subtitle: "React",
+    description:
+      "Esta fue la primer pagina web personal que hice",
+    image: "./myPage.png",
+    link: "",
+  },
+  {
+    title: "React and Javascript",
+    subtitle: "Fucesa login",
+    description:
+      "Estuve trabajando un tiempo en el desarrollo de una pagina para una empresa llamada Fucesa",
+    image: "./fucesaLogin.png",
+    link: "",
+  }
+];
+
+export const testimonials = [
+  {
+    quote:
+      "El trabajo de Charlie es impecable y este es un comentario fake",
+    image: "https://randomuser.me/api/portraits/men/1.jpg",
+    name: "Jon",
+    company: "Google",
+  },
+  {
+    quote:
+      "Excelente servicio, este comentario también es fake",
+    image: "https://randomuser.me/api/portraits/men/94.jpg",
+    name: "Frank",
+    company: "Teia",
+  },
+];
+
+export const skills = [
+  "JavaScript",
+  "React",
+  "Next.js",
+  "Node",
+  "Phyton",
+  "C++",
+];
